06c445eb5f69841623f3798158d5f40b05045472
Gowtham Chandrasekaran <gowthamdhss@gmail.com>
2020-03-29 12:58:31 +0530

    Final Build

--- src/components/Introsection.js
+++ src/components/Introsection.js
@@ -4,7 +4,7 @@ function introsection(){
  return (
      <div className="">
      <div >
-     <p className="text-4xl text-gray-800 f1 lg:text-lg">Hi,I'm <span className="text-black font-semibold text-5xl lg:text-xl">Gowtham</span></p>
+     <p className="text-4xl text-gray-800 f1 lg:text-lg">Hi, I'm <span className="text-black font-semibold text-5xl lg:text-xl">Gowtham</span></p>
      </div>
      <div className="mt-4">
          <p className="text-6xl font-semibold lg:font-medium leading-snug f3 lg:text-3xl">Full Stack Web Developer</p>
