5d60d2f26948ef368956b60b865ffd80cd671752
Gowtham Chandrasekaran <gowthamdhss@gmail.com>
2020-04-01 17:39:07 +0530

    Updates

---
+++ precache-manifest.804347f524238931e321b46162bd76ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbb7faad6a776e28d5f9db5737b59c3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "73ee92da53395d745efa",
+    "url": "/portfolio/static/css/main.9a0dea0c.chunk.css"
+  },
+  {
+    "revision": "db1f86d7ed3e41908277",
+    "url": "/portfolio/static/js/2.82a0fc8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.82a0fc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ee92da53395d745efa",
+    "url": "/portfolio/static/js/main.96f33a56.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
